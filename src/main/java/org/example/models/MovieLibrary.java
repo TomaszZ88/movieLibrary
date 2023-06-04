952f8fd26599e6abf12bce8873669068a0fe61d9
Tomasz Zawadzki <tomasz.zawadzki.tz1@contractors.roche.com>
2023-06-04 12:56:45 +0200

    add Actor class

--- src/main/java/org/example/models/MovieLibrary.java
+++ src/main/java/org/example/models/MovieLibrary.java
@@ -1,6 +1,6 @@
-package org.example;
+package org.example.models;
 
-public class Main {
+public class MovieLibrary {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
